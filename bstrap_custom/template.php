1c38b592df8a8b4c9861b763909e5fe467aafa70
darnolf <arnolf10@gmail.com>
2018-06-10 01:14:28 +0300

    missing commit

--- bstrap_custom/template.php
+++ bstrap_custom/template.php
@@ -14,7 +14,6 @@
  */
 
  function bstrap_custom_views_view_field__bootstrap__page__uri ($variables) {
-   //kpr($variables['output']);
    $output = '<div style="background: red;">'.$variables['output'].'</div>';
    return ($output);
  }
